b12840bbbf213df858d8b37d1e38f3a5e5bf0a34
mac <94307584+Vitaliy28031985@users.noreply.github.com>
2022-05-24 12:41:28 +0300

    geleri

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -1,2 +1,16 @@
 import axios from 'axios';
 
+export default async function fetchImages(value, page) {
+
+   const url = 'https://pixabay.com/api/';
+   
+   const perPage = '40';
+   
+   const key = '27545952-886edf6444d41f28a84ae17ec';
+   
+   const filter = `?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`;
+   
+   return await axios.get(`${url}${filter}`).then(response => response.data);
+   
+   }
+   
